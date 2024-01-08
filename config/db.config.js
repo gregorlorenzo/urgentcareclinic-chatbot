95922a51c04bd3369d126d0f913916f14de440cd
Gregor Labadan <gregorlorenzolabadan@gmail.com>
2024-01-08 12:20:45 +0800

    secured api keys

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://admin1:jYF7noTBqzG19VMa@urgentcareclinicbot.jpkrl.mongodb.net/urgentcareclinicdb?retryWrites=true&w=majority"
+    url: process.env.DATABASE_URI
 };
